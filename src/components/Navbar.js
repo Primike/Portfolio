75115247e50783d488fa652749a7272f748f72ef
Prince <avecillasprince@yahoo.com>
2022-02-04 13:41:02 -0500

    Done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Menu } from 'semantic-ui-react'
 
-export default class MenuExampleBasic extends Component {
-  state = {}
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
-  render() {
-    const { activeItem } = this.state
+function Navbar () {
+    
+    const items = [
+        { key: 'editorials', name: 'Home',},
+        { key: 'review', name: 'About' },
+        { key: 'events', name: 'Projects' },
+      ]
 
     return (
-      <Menu>
-        <Menu.Item
-          name='editorials'
-          active={activeItem === 'editorials'}
-          onClick={this.handleItemClick}
-        >
-          Editorials
-        </Menu.Item>
-
-        <Menu.Item
-          name='reviews'
-          active={activeItem === 'reviews'}
-          onClick={this.handleItemClick}
-        >
-          Reviews
-        </Menu.Item>
-
-        <Menu.Item
-          name='upcomingEvents'
-          active={activeItem === 'upcomingEvents'}
-          onClick={this.handleItemClick}
-        >
-          Upcoming Events
-        </Menu.Item>
-      </Menu>
+        <>
+            <Menu items={items} />
+        </>
     )
-  }
 }
+
+
+export default Navbar
