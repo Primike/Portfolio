d67281b76706ee6b23302972ed7379653a5e7baa
Prince Avecillas <avecillasprince@yahoo.com>
2022-02-06 16:13:39 -0500

    Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from  "./components/Home"
+import About from "./components/About"
+import Projects from "./components/Projects"
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/about" element={<About />} />
+              <Route path="/projects" element={<Projects />} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Menu } from 'semantic-ui-react'
 
-function Navbar () {
-    
-    const items = [
-        { key: 'editorials', name: 'Home',},
-        { key: 'review', name: 'About' },
-        { key: 'events', name: 'Projects' },
-      ]
+const Navbar = () => {
 
-    return (
-        <>
-            <Menu items={items} />
-        </>
-    )
-}
+  const [activeItem, setActiveItem] = useState('Accordion');
 
+  const handleItemClick = (name) => setActiveItem(name)
 
-export default Navbar
+  return (
+    <Menu>
+        <Menu.Item header>
+            <img src = "https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Foriginal%2F000%2F013%2F564%2Fdoge.jpg"/>
+        </Menu.Item>
+        <Menu.Item  
+            href = "/"      
+            name = 'Home'
+            active = {activeItem === 'Home'}
+            onClick = {() => handleItemClick('Home')}
+        />
+        <Menu.Item  
+            href = "/about"       
+            name = 'About'
+            active = {activeItem === 'About'}
+            onClick = {() => handleItemClick('About')} 
+        />
+        <Menu.Item     
+            href = "/projects" 
+            name ='Projects'
+            active = {activeItem === 'Projects'}
+            onClick = {() => handleItemClick('Projecs')} 
+        />
+      </Menu>
+  );
+};
+
+export default Navbar;
