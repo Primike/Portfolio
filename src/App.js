bbc2494f59baba458de4912b4540c8ecb76f3cc8
Prince Avecillas <avecillasprince@yahoo.com>
2022-02-06 18:56:40 -0500

    Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Navbar from './components/Navbar';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from  "./components/Home"
 import About from "./components/About"
 import Projects from "./components/Projects"
